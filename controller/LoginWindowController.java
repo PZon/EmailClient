ecfacab502348e20e288e05fe380647fa01e3768
Pzon <piotrzon@gmail.com>
2020-09-17 14:22:30 +0200

    basic main window setup

--- controller/LoginWindowController.java
+++ controller/LoginWindowController.java
@@ -1,4 +1,4 @@
-package emailClient.view;
+package emailClient.controller;
 
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
---
+++ controller/MainWindowController.java
@@ -0,0 +1,26 @@
+package emailClient.controller;
+
+/**
+ * Created by PZON_SM on 17.09.2020.
+ **/
+import javafx.fxml.FXML;
+import javafx.scene.control.TableView;
+import javafx.scene.control.TreeView;
+import javafx.scene.web.WebView;
+
+public class MainWindowController {
+
+    @FXML
+    private TreeView<?> emailTreeView;
+
+    @FXML
+    private TableView<?> emailTableView;
+
+    @FXML
+    private WebView emailWebView;
+
+    @FXML
+    void menuOptionAction() {
+
+    }
+}
