eabe36217bb00b7439cd15fcf7e0022e01a3b807
Pzon <piotrzon@gmail.com>
2020-09-23 13:58:49 +0200

    End section 3 working css features

--- controller/OptionsWindowController.java
+++ controller/OptionsWindowController.java
@@ -10,6 +10,7 @@ import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.control.ChoiceBox;
 import javafx.scene.control.Slider;
+import javafx.stage.Stage;
 import javafx.util.StringConverter;
 
 import java.net.URL;
@@ -33,12 +34,15 @@ public class OptionsWindowController extends BaseController implements Initializ
 
     @FXML
     void applyButtonAction() {
-
+        viewFactory.setColorTheme(themePicker.getValue());
+        viewFactory.setFontSize(FontSize.values()[(int)(fontSizePicker.getValue())]);
+        viewFactory.updateStyle();
     }
 
     @FXML
     void cancelButtonAction() {
-
+        Stage stage = (Stage) fontSizePicker.getScene().getWindow();
+        viewFactory.closeStage(stage);
     }
 
     @Override
